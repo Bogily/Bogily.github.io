a6721af65a0eb72e689d06dcc352748dba370651
bogily <126955759+Bogily@users.noreply.github.com>
2024-11-20 20:38:06 +0100

    Added pfp and banner and fixed some stuff (i did bunch of stuff)
    -things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backdropSaturate: {
+        80: '.8',
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      },
+      backdropBlur: {
+        s2xl: '32px',
+      }
+    },
   },
   plugins: [],
 }

8f9ff96c29ff979d17cbf6e438773ce62f270b7b
bogily <126955759+Bogily@users.noreply.github.com>
2024-11-20 19:21:43 +0100

    recode

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    const parallaxElements = document.querySelectorAll('.navbar, .main-content');
-    const x = (e.clientX / window.innerWidth - 0.5) * 100;
-    const y = (e.clientY / window.innerHeight - 0.5) * 100;
-
-    parallaxElements.forEach(element => {
-        element.style.transform = `translate(${x}px, ${y}px)`;
-    });
-});

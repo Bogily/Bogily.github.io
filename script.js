e2e09b36f408874f441674576fe2b4bea6c87df6
Bogily <126955759+Bogily@users.noreply.github.com>
2024-11-20 12:50:36 +0100

    nvm lets use it

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('mousemove', function(e) {
     const parallaxElements = document.querySelectorAll('.navbar, .main-content');
-    const x = (e.clientX / window.innerWidth - 0.5) * 0;
-    const y = (e.clientY / window.innerHeight - 0.5) * 0;
+    const x = (e.clientX / window.innerWidth - 0.5) * 100;
+    const y = (e.clientY / window.innerHeight - 0.5) * 100;
 
     parallaxElements.forEach(element => {
         element.style.transform = `translate(${x}px, ${y}px)`;

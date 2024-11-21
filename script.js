483d2421083301e8726e15c28280b1068ce14cfc
Bogily <126955759+Bogily@users.noreply.github.com>
2024-11-21 09:19:32 +0100

    feat: local storage for the tv-button

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById('TV-Toggle').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  const tvToggleButton = document.getElementById('TV-Toggle');
+  const overlayState = localStorage.getItem('overlayState');
+
+  if (overlayState === 'off') {
+    document.body.classList.add('no-overlay');
+  }
+
+  tvToggleButton.addEventListener('click', function() {
     document.body.classList.toggle('no-overlay');
+    const isOverlayOff = document.body.classList.contains('no-overlay');
+    localStorage.setItem('overlayState', isOverlayOff ? 'off' : 'on');
   });
+});
